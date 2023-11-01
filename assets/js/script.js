342960fbf38fb2ce90f546cd97c251383a22a2f3
Prajwal T M <95957040+PrajwalTM@users.noreply.github.com>
2023-11-01 15:12:12 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["Cyber Security Consultant", "Red Team Analyst", "Purple Team Analyst", "CTF Player", "CEH v12 Practical"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
